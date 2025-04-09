1c91b14306e96e25f572f8c1108d47c395df3105
Lucas-blancaws <lblancportier@gmail.com>
2025-04-09 14:19:19 +0200

    fixxxx task 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,13 +9,18 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	size_t left_count = 0, right_count = 0;
 
 	if (tree == NULL)
+	{
 		return (0);
+	}
+		
 
-		if (tree->left == NULL && tree->right == NULL)
+	if (tree->left == NULL && tree->right == NULL)
+	{
 		return (0);
-
-			left_count = binary_tree_nodes(tree->left);
-			right_count = binary_tree_nodes(tree->right);
+	}
 			
-		return (1 + left_count + right_count);
+	left_count = binary_tree_nodes(tree->left);
+	right_count = binary_tree_nodes(tree->right);
+
+	return (1 + left_count + right_count);
 }
