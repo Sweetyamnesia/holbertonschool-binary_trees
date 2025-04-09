8e8bba6f55a3eb057dcb40289790095c28027a6b
Sweetyamnesia <angela.r@live.fr>
2025-04-09 13:06:50 +0200

    update file : add condiiton

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <stddef.h>
+#include <math.h>
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - measures the balance factor of a binary tree
+ * binary_tree_balance - calculates the balance factor of a binary tree
  * @tree: pointer to the root node of the tree to measure the balance factor
  * Return: 0 if tree is NULL
  */
@@ -15,8 +16,12 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 	return (0);
 
+	if (tree->left == NULL && tree->right == NULL)
+	return (0);
+
 	lHeight = binary_tree_height(tree->left);
 	rHeight = binary_tree_height(tree->right);
 
 	return (lHeight - rHeight);
+
 }
