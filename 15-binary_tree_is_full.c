c2d94e27a7cbac05b1e115dd64d2fb654cc386da
Lucas-blancaws <lblancportier@gmail.com>
2025-04-09 15:08:48 +0200

    task 15

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,25 +1,25 @@
 #include "binary_trees.h"
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    int left;
-    int right;
+	int left;
+	int right;
 
-    if (tree == NULL)
-    return (0);
-    if (tree->left == NULL && tree->right == NULL)
-    return (1);
-    left = binary_tree_is_full(tree->left);
-    right = binary_tree_is_full(tree->right);
-    if (tree->left != NULL && tree->right != NULL)
-    {
-        if (left == 1 && right == 1)
-        {
-            return (1);
-        }
-        else
-        {
-            return (0);
-        }
-    }
-    return (0);
+	if (tree == NULL)
+	return (0);
+	if (tree->left == NULL && tree->right == NULL)
+	return (1);
+	left = binary_tree_is_full(tree->left);
+	right = binary_tree_is_full(tree->right);
+	if (tree->left != NULL && tree->right != NULL)
+	{
+		if (left == 1 && right == 1)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (0);
 }
