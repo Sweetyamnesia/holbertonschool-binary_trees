15b7703efd4cad87f7934b67170e7871dacebafa
Lucas-blancaws <lblancportier@gmail.com>
2025-04-09 09:35:45 +0000

    task 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,12 +1,12 @@
 #include "binary_trees.h"
 /**
- * binary_tree_size - Calcule la taille (nombre total de noeud d'un arbre binaire)
+ * binary_tree_size - Calcule la taille (nombre total de noeud d'un arbre)
  * @tree: pointeur vers la racine de l'arbre
  * Return: taille de l'arbre (0 si tree est NULL)
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	if (tree == NULL)	
+	if (tree == NULL)
 	{
 		return (0);
 	}
